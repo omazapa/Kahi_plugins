34b27e99d1cfcf465b78d9da22e96ad12ecdf32f
muzgash <muzgash@gmail.com>
2023-08-02 15:07:58 -0500

    Fixed the cursor not found error by casting the cursor to a list since the information is not so big

--- Kahi_openalex_subjects/kahi_openalex_subjects/Kahi_openalex_subjects.py
+++ Kahi_openalex_subjects/kahi_openalex_subjects/Kahi_openalex_subjects.py
@@ -90,76 +90,66 @@ class Kahi_openalex_subjects(KahiBase):
                     self.relations_inserted_ids.append(oa_id)
 
     def process_openalex(self):
-        with self.openalex_client.start_session() as session:
-            self.openalex_db = self.openalex_client[self.config["openalex_subjects"]
-                                                    ["database_name"]]
-            self.openaelex_collection = self.openalex_db[self.config["openalex_subjects"]
-                                                         ["collection_name"]]
-            old = dt.datetime.now()
-            for sub in self.openalex_collection.find({"id": {"$nin": self.inserted_concepts}}):
-                if sub["id"] in self.inserted_concepts:
-                    continue
-                db_reg = self.collection.find_one(
-                    {"external_ids.id": sub["id"]})
-                if db_reg:
-                    self.inserted_concepts.append(sub["id"])
-                    self.inserted_concepts_ids_tuples.append(
-                        (db_reg["_id"], sub["id"]))
-                    continue
-                entry = self.empty_subjects()
-                entry["updated"] = [
-                    {"source": "openalex", "time": int(time())}]
-                sources_inserted_ids = []
-                entry["external_ids"].append(
-                    {"source": "openalex", "id": sub["id"]})
-                sources_inserted_ids.append("openalex")
-                for source, idx in sub["ids"].items():
-                    if source in sources_inserted_ids:
-                        continue
-                    entry["external_ids"].append({"source": source, "id": idx})
-                    sources_inserted_ids.append(source)
-                entry["level"] = sub["level"]
-                entry["names"].append(
-                    {"name": sub["display_name"], "lang": "en"})
-                inserted_lang_names = ["en"]
-                if sub["international"]:
-                    if sub["international"]["display_name"]:
-                        for lang, name in sub["international"]["display_name"].items():
-                            if lang in inserted_lang_names:
-                                continue
-                            entry["names"].append({"name": name, "lang": lang})
-                            inserted_lang_names.append(lang)
-                if sub["description"]:
-                    entry["descriptions"].append(
-                        {"description": sub["description"], "lang": "en"})
-                if sub["wikidata"]:
-                    entry["external_urls"].append(
-                        {"source": "wikidata", "url": sub["wikidata"]})
-                if sub["image_url"]:
-                    entry["external_urls"].append(
-                        {"source": "image", "url": sub["image_url"]})
-
-                response = self.collection.insert_one(entry)
+        openalex_subjects = list(self.openalex_collection.find({"id": {"$nin": self.inserted_concepts}}))
+        for sub in openalex_subjects:
+            if sub["id"] in self.inserted_concepts:
+                continue
+            db_reg = self.collection.find_one(
+                {"external_ids.id": sub["id"]})
+            if db_reg:
                 self.inserted_concepts.append(sub["id"])
                 self.inserted_concepts_ids_tuples.append(
-                    (response.inserted_id, sub["id"]))
+                    (db_reg["_id"], sub["id"]))
+                continue
+            entry = self.empty_subjects()
+            entry["updated"] = [
+                {"source": "openalex", "time": int(time())}]
+            sources_inserted_ids = []
+            entry["external_ids"].append(
+                {"source": "openalex", "id": sub["id"]})
+            sources_inserted_ids.append("openalex")
+            for source, idx in sub["ids"].items():
+                if source in sources_inserted_ids:
+                    continue
+                entry["external_ids"].append({"source": source, "id": idx})
+                sources_inserted_ids.append(source)
+            entry["level"] = sub["level"]
+            entry["names"].append(
+                {"name": sub["display_name"], "lang": "en"})
+            inserted_lang_names = ["en"]
+            if sub["international"]:
+                if sub["international"]["display_name"]:
+                    for lang, name in sub["international"]["display_name"].items():
+                        if lang in inserted_lang_names:
+                            continue
+                        entry["names"].append({"name": name, "lang": lang})
+                        inserted_lang_names.append(lang)
+            if sub["description"]:
+                entry["descriptions"].append(
+                    {"description": sub["description"], "lang": "en"})
+            if sub["wikidata"]:
+                entry["external_urls"].append(
+                    {"source": "wikidata", "url": sub["wikidata"]})
+            if sub["image_url"]:
+                entry["external_urls"].append(
+                    {"source": "image", "url": sub["image_url"]})
 
-                delta = dt.datetime.now() - old
-                if delta.seconds > 240:
-                    self.openalex_client.admin.command(
-                        'refreshSessions', [session.session_id], session=session)
-                    old = dt.datetime.now()
+            response = self.collection.insert_one(entry)
+            self.inserted_concepts.append(sub["id"])
+            self.inserted_concepts_ids_tuples.append(
+                (response.inserted_id, sub["id"]))
 
     def process_relations(self):
+        openalex_data=list(self.openalex_collection.find({"id": {"$nin": self.relations_inserted_ids}},{"id":1,"ancestors":1,"related_concepts":1}))
         Parallel(
             n_jobs=self.n_jobs,
             backend="multiprocessing",
             verbose=10
-        )(delayed(process_relation)(sub, self.config["database_url"], self.config["database_name"]) for sub in self.openalex_collection.find({"id": {"$nin": self.relations_inserted_ids}}))
+        )(delayed(process_relation)(sub, self.config["database_url"], self.config["database_name"]) for sub in openalex_data)
 
     def run(self):
-        # print("Inserting the subjects")
-        # self.process_openalex()
+        print("Inserting the subjects")
+        self.process_openalex()
         print("Creating relations")
         self.process_relations()
         return 0
