5daa04d8bb21f45ef8a66cdb2ecb9feb1a485c01
muzgash <muzgash@gmail.com>
2023-08-08 17:59:00 -0500

    Added new way to receive the configuration. Added README. Added required packages in setup

--- Kahi_ror_affiliations/README.md
+++ Kahi_ror_affiliations/README.md
@@ -1,43 +1,43 @@
 <center><img src="https://raw.githubusercontent.com/colav/colav.github.io/master/img/Logo.png"/></center>
 
-# Kahi template plugin 
-This is a template for xyz project
-replace template for the name of the plugin everywhere.
+# Kahi ROR affiliations plugin 
+Kahi will use this plugin to insert or update the institutional information from ROR database.
 
 # Description
-Write something meaningful here ;)
+Plugin that reads the information from a mongodb collection with ROR data dump to update or insert the information of its institutions in CoLav's database format.
 
 # Installation
+You could download the repository from github. Go into the folder where the setup.py is located and run
+```shell
+pip3 install .
+```
+From the package you can install by running
+```shell
+pip3 install kahi_ror_affiliations
+```
 
 ## Dependencies
-What do I need fot this plugin?, it could be external services etc..
-
-## Package
-Write here how to install this plugin
-usauly is 
-
-`pip install kahi_template`
-
+Software dependencies will automatically be installed when installing the plugin.
+The user must have a copy of the DOAJ dump which can be downloaded at [ROR data](https://zenodo.org/communities/ror-data "ROR data") and import it on a mongodb database.
 
 # Usage
-what should I know?
-put it here.
-
-Additional parameters for kahi_run in the workflow should be here as well.
-example :
-
+To use this plugin you must have kahi installed in your system and construct a yaml file such as
+```yaml
+config:
+  database_url: localhost:27017
+  database_name: kahi
+  log_database: kahi_log
+  log_collection: log
+workflow:
+  ror_affiliations:
+    database_url: localhost:27017
+    database_name: ror
+    collection_name: stage
+    num_jobs: 10
 ```
-template:
-    -   my_param_example: value
-```
-Those parameters are not really needed in the workflow file, it is just for illustration.
-
 
 # License
 BSD-3-Clause License 
 
 # Links
 http://colav.udea.edu.co/
-
-
-
--- Kahi_ror_affiliations/kahi_ror_affiliations/Kahi_ror_affiliations.py
+++ Kahi_ror_affiliations/kahi_ror_affiliations/Kahi_ror_affiliations.py
@@ -1,5 +1,6 @@
 from kahi.KahiBase import KahiBase
 from pymongo import MongoClient
+from time import time
 
 
 class Kahi_ror_affiliations(KahiBase):
@@ -8,107 +9,89 @@ class Kahi_ror_affiliations(KahiBase):
 
     def __init__(self, config):
         self.config = config
-        self.mongodb_url = config["mongodb_url"]
-        
-        self.client=MongoClient(self.mongodb_url)
-        
-        self.db=client[config["database_name"]]
-        self.collection=db["affiliations"]
+        self.mongodb_url = config["database_url"]
 
-        self.ror_client=MongoClient(config["plugin"]["mongodb_url"])
+        self.client = MongoClient(self.mongodb_url)
+        self.db = self.client[config["database_name"]]
+        self.collection = self.db["affiliations"]
 
-        self.ror_db=client[config["plugin"]["database_name"]]
-        self.ror_collection=db[config["plugin"]["collection_name"]]
+        self.ror_client = MongoClient(config["ror_affiliations"]["database_url"])
+        self.ror_db = self.ror_client[config["ror_affiliations"]["database_name"]]
+        self.ror_collection = self.ror_db[config["ror_affiliations"]["collection_name"]]
 
-        #logs for higher verbosity
-        self.already_in_db=[]
-        self.inserted=[]
+        # logs for higher verbosity
+        self.already_in_db = []
+        self.inserted = []
 
-    def empty_affiliations(self):
-        entry = {
-            "updated":[],
-            "names":[],
-            "aliases":[],
-            "abbreviations":[],
-            "types":[],
-            "year_established":None,
-            "status":[],
-            "relations":[],
-            "addresses":[],
-            "external_urls":[],
-            "external_ids":[],
-            "subjects":[],
-            "ranking":[],
-            "description":[]
-        }
-        return entry
-
-    def process_ror(self,verbose=0):
+    def process_ror(self, verbose=0):
         for inst in self.ror_collection.find():
-            found_entry=self.collection.find_one({"external_ids.id":inst["id"]})
+            found_entry = self.collection.find_one(
+                {"external_ids.id": inst["id"]})
             if found_entry:
                 self.already_in_db.append(inst["id"])
-                if verbose>4:
-                    print("Already in db: "+inst["id"])
-                if verbose >0:
-                    print("Total already found: "+len(self.already_in_db))
+                if verbose > 4:
+                    print("Already in db: ",inst["id"])
+                if verbose > 0:
+                    print("Total already found: ",len(self.already_in_db))
                 continue
-                #may be updatable, check accordingly
+                # may be updatable, check accordingly
             else:
-                entry=self.empty_affiliations()
-                entry["updated"].append({"time":int(time()),"source":"ror"})
-                entry["names"].append({"name":inst["name"],"lang":"en"})
+                entry = self.empty_affiliations()
+                entry["updated"].append({"time": int(time()), "source": "ror"})
+                entry["names"].append({"name": inst["name"], "lang": "en"})
                 entry["aliases"].extend(inst["aliases"])
                 entry["abbreviations"].extend(inst["acronyms"])
-                entry["year_established"]=int(inst["established"]) if inst["established"] else -1
-                entry["status"]=[inst["status"]]
-                
-                #types
+                entry["year_established"] = int(
+                    inst["established"]) if inst["established"] else -1
+                entry["status"] = [inst["status"]]
+
+                # types
                 for typ in inst["types"]:
-                    entry["types"].append({"source":"ror","type":typ})
-                
-                #addresses
+                    entry["types"].append({"source": "ror", "type": typ})
+
+                # addresses
                 for add in inst["addresses"]:
-                    add_entry={
-                        "lat":add["lat"],
-                        "lng":add["lng"],
-                        "postcode":add["postcode"] if add["postcode"] else "",
-                        "state":add["state"],
-                        "city":add["city"],
-                        "country":"",
-                        "country_code":"",
+                    add_entry = {
+                        "lat": add["lat"],
+                        "lng": add["lng"],
+                        "postcode": add["postcode"] if add["postcode"] else "",
+                        "state": add["state"],
+                        "city": add["city"],
+                        "country": "",
+                        "country_code": "",
                     }
                     entry["addresses"].append(add_entry)
-                entry["addresses"][0]["country"]=inst["country"]["country_name"]
-                entry["addresses"][0]["country_code"]=inst["country"]["country_code"]
-                
-                #external_urls
+                entry["addresses"][0]["country"] = inst["country"]["country_name"]
+                entry["addresses"][0]["country_code"] = inst["country"]["country_code"]
+
+                # external_urls
                 if inst["links"]:
                     for link in inst["links"]:
-                        url_entry={"source":"site","url":inst["links"][0]}
-                        if not url_entry in entry["external_urls"]:
+                        url_entry = {"source": "site", "url": inst["links"][0]}
+                        if url_entry not in entry["external_urls"]:
                             entry["external_urls"].append(url_entry)
                 if inst["wikipedia_url"]:
-                    entry["external_urls"].append({"source":"wikipedia","url":inst["wikipedia_url"]})
+                    entry["external_urls"].append(
+                        {"source": "wikipedia", "url": inst["wikipedia_url"]})
 
-                #external_ids
+                # external_ids
                 if inst["external_ids"]:
-                    for key,ext in inst["external_ids"].items():
-                        if isinstance(ext["all"],list):
-                            alll=ext["all"][0] if len(ext["all"])>0 else ext["all"]
-                            ext_entry={"source":key.lower(),"id":alll}
-                            if not ext_entry in entry["external_ids"]:
+                    for key, ext in inst["external_ids"].items():
+                        if isinstance(ext["all"], list):
+                            alll = ext["all"][0] if len(
+                                ext["all"]) > 0 else ext["all"]
+                            ext_entry = {"source": key.lower(), "id": alll}
+                            if ext_entry not in entry["external_ids"]:
                                 entry["external_ids"].append(ext_entry)
-                entry["external_ids"].append({"source":"ror","id":inst["id"]})
-            response=self.collection.insert_one(entry)
+                entry["external_ids"].append(
+                    {"source": "ror", "id": inst["id"]})
+            response = self.collection.insert_one(entry)
             self.inserted.append(response.inserted_id)
-            if verbose>4:
-                print("Inserted: "+response.inserted_id)
-            if verbose >0:
-                print("Total inserted: "+len(self.inserted))
-
-
-
+            if verbose > 4:
+                print("Inserted: ",response.inserted_id)
+            if verbose > 0:
+                print("Total inserted: ",len(self.inserted))
 
     def run(self):
-        process_ror(verbose=5)
+        self.process_ror(verbose=5)
+        return 0
