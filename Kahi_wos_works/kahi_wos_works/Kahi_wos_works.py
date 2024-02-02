d2337a45e6488f77f4cfd68879951c3b6b789c9d
Omar Zapata <Omar.Zapata@cern.ch>
2024-02-02 11:07:59 -0500

    [Kahi_wos_works] implemented kahi_impactu_utils

--- Kahi_wos_works/kahi_wos_works/Kahi_wos_works.py
+++ Kahi_wos_works/kahi_wos_works/Kahi_wos_works.py
@@ -2,184 +2,9 @@ from kahi.KahiBase import KahiBase
 from pymongo import MongoClient, TEXT
 from time import time
 from joblib import Parallel, delayed
-from re import sub, split, search, UNICODE
-import unidecode
 from thefuzz import fuzz
 
-from langid import classify
-import pycld2 as cld2
-from langdetect import DetectorFactory, PROFILES_DIRECTORY
-from fastspell import FastSpell
-from lingua import LanguageDetectorBuilder
-import iso639
-
-fast_spell = FastSpell("en", mode="cons")
-
-
-def lang_poll(text, verbose=0):
-    text = text.lower()
-    text = text.replace("\n", "")
-    lang_list = []
-
-    lang_list.append(classify(text)[0].lower())
-
-    detected_language = None
-    try:
-        _, _, _, detected_language = cld2.detect(text, returnVectors=True)
-    except Exception as e:
-        if verbose > 4:
-            print("Language detection error using cld2, trying without ascii")
-            print(e)
-        try:
-            text = str(unidecode.unidecode(text).encode("ascii", "ignore"))
-            _, _, _, detected_language = cld2.detect(text, returnVectors=True)
-        except Exception as e:
-            if verbose > 4:
-                print("Language detection error using cld2")
-                print(e)
-
-    if detected_language:
-        lang_list.append(detected_language[0][-1].lower())
-
-    try:
-        _factory = DetectorFactory()
-        _factory.load_profile(PROFILES_DIRECTORY)
-        detector = _factory.create()
-        detector.append(text)
-        lang_list.append(detector.detect().lower())
-    except Exception as e:
-        if verbose > 4:
-            print("Language detection error using langdetect")
-            print(e)
-
-    try:
-        result = fast_spell.getlang(text)  # low_memory breaks the function
-        lang_list.append(result.lower())
-    except Exception as e:
-        if verbose > 4:
-            print("Language detection error using fastSpell")
-            print(e)
-
-    detector = LanguageDetectorBuilder.from_all_languages().build()
-    res = detector.detect_language_of(text)
-    if res:
-        if res.name.capitalize() == "Malay":
-            la = "ms"
-        elif res.name.capitalize() == "Sotho":
-            la = "st"
-        elif res.name.capitalize() == "Bokmal":
-            la = "no"
-        elif res.name.capitalize() == "Swahili":
-            la = "sw"
-        elif res.name.capitalize() == "Nynorsk":
-            la = "is"
-        elif res.name.capitalize() == "Slovene":
-            la = "sl"
-        else:
-            la = iso639.find(
-                res.name.capitalize())["iso639_1"].lower()
-        lang_list.append(la)
-
-    lang = None
-    for prospect in set(lang_list):
-        votes = lang_list.count(prospect)
-        if votes > len(lang_list) / 2:
-            lang = prospect
-            break
-    return lang
-
-
-def split_names(s, exceptions=['GIL', 'LEW', 'LIZ', 'PAZ', 'REY', 'RIO', 'ROA', 'RUA', 'SUS', 'ZEA']):
-    """
-    Extract the parts of the full name `s` in the format ([] → optional):
-
-    [SMALL_CONECTORS] FIRST_LAST_NAME [SMALL_CONECTORS] [SECOND_LAST_NAME] NAMES
-
-    * If len(s) == 2 → Foreign name assumed with single last name on it
-    * If len(s) == 3 → Colombian name assumed two last mames and one first name
-
-    Add short last names to `exceptions` list if necessary
-
-    Works with:
-    ----
-        s='LA ROTTA FORERO DANIEL ANDRES'
-        s='MONTES RAMIREZ MARIA DEL CONSUELO'
-        s='CALLEJAS POSADA RICARDO DE LA MERCED'
-        s='DE LA CUESTA BENJUMEA MARIA DEL CARMEN'
-        s='JARAMILLO OCAMPO NICOLAS CARLOS MARTI'
-        s='RESTREPO QUINTERO DIEGO ALEJANDRO'
-        s='RESTREPO ZEA JAIRO HUMBERTO'
-        s='JIMENEZ DEL RIO MARLEN'
-        s='RESTREPO FERNÁNDEZ SARA' # Colombian: two LAST_NAMES NAME
-        s='NARDI ENRICO' # Foreing
-    Fails:
-    ----
-        s='RANGEL MARTINEZ VILLAL ANDRES MAURICIO' # more than 2 last names
-        s='ROMANO ANTONIO ENEA' # Foreing → LAST_NAME NAMES
-    """
-    s = s.title()
-    exceptions = [e.title() for e in exceptions]
-    sl = sub('(\s\w{1,3})\s', r'\1-', s, UNICODE)  # noqa: W605
-    sl = sub('(\s\w{1,3}\-\w{1,3})\s', r'\1-', sl, UNICODE)  # noqa: W605
-    sl = sub('^(\w{1,3})\s', r'\1-', sl, UNICODE)  # noqa: W605
-    # Clean exceptions
-    # Extract short names list
-    lst = [s for s in split(
-        '(\w{1,3})\-', sl) if len(s) >= 1 and len(s) <= 3]  # noqa: W605
-    # intersection with exceptions list
-    exc = [value for value in exceptions if value in lst]
-    if exc:
-        for e in exc:
-            sl = sl.replace('{}-'.format(e), '{} '.format(e))
-
-    # if sl.find('-')>-1:
-    # print(sl)
-    sll = [s.replace('-', ' ') for s in sl.split()]
-    if len(s.split()) == 2:
-        sll = [s.split()[0]] + [''] + [s.split()[1]]
-    #
-    d = {'NOMBRE COMPLETO': ' '.join(sll[2:] + sll[:2]),
-         'PRIMER APELLIDO': sll[0],
-         'SEGUNDO APELLIDO': sll[1],
-         'NOMBRES': ' '.join(sll[2:]),
-         'INICIALES': ' '.join([i[0] + '.' for i in ' '.join(sll[2:]).split()])
-         }
-    return d
-
-
-def dois_processor(doi):
-    """
-    Process a DOI (Digital Object Identifier) and return a cleaned version.
-    Args:
-        doi (str): The DOI to be processed.
-    Returns:
-        str or bool: If a valid DOI is found, return the cleaned DOI; otherwise, return False.
-    """
-    doi_regex = r"\b10\.\d{4,}/[^\s]+"
-    match = search(doi_regex, doi)
-    if match:
-        return match.group().strip().strip('.')
-    doi_candidate = doi.replace(" ", "")
-    match = search(doi_regex, doi_candidate)
-    if match:
-        return match.group().strip().strip('.')
-    if ('http' in doi_candidate or 'www' in doi_candidate) and "10." in doi_candidate:
-        doi_candidate = doi_candidate.split("/10")[-1].replace("%2f", "/")
-        doi_candidate = "10" + doi_candidate
-        match = search(doi_regex, doi_candidate)
-        if match:
-            return match.group().strip('.')
-    doi_candidate = doi.split("/")
-    if doi_candidate[0].endswith('.'):
-        doi_candidate[0] = doi_candidate[0].strip('.')
-    if "." not in doi_candidate[0]:
-        doi_candidate[0] = doi_candidate[0].replace("10", "10.")
-    doi_candidate = '/'.join(doi_candidate)
-    match = search(doi_regex, doi_candidate)
-    if match:
-        return match.group().strip().strip('.')
-
-    return False
+from kahi_impactu_utils.Utils import doi_processor, lang_poll
 
 
 def parse_wos(reg, empty_work, verbose=0):
@@ -227,7 +52,7 @@ def parse_wos(reg, empty_work, verbose=0):
 
     if "DI" in reg.keys():
         if reg["DI"]:
-            ext = {"source": "doi", "id": dois_processor(reg["DI"]).lower()}
+            ext = {"source": "doi", "id": doi_processor(reg["DI"])}
             entry["external_ids"].append(ext)
     if "UT" in reg.keys():
         if reg["UT"]:
@@ -363,7 +188,7 @@ def process_one(wos_reg, db, collection, empty_work, verbose=0):
     # register has doi
     if wos_reg["DI"]:
         if isinstance(wos_reg["DI"], str):
-            doi = dois_processor(wos_reg["DI"]).lower()
+            doi = doi_processor(wos_reg["DI"])
     if doi:
         # is the doi in colavdb?
         colav_reg = collection.find_one({"external_ids.id": doi})
