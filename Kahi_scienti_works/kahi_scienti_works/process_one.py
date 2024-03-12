2675132c1efab795b11372fbf30fc6f44a1c0385
Omar Zapata <andresete.chaos@gmail.com>
2024-03-12 11:56:28 -0500

    [Kahi_scienti_works] added provenance to ES

--- Kahi_scienti_works/kahi_scienti_works/process_one.py
+++ Kahi_scienti_works/kahi_scienti_works/process_one.py
@@ -410,6 +410,8 @@ def process_one_insert(scienti_reg, db, collection, empty_work, es_handler, verb
             if "full_name" in author.keys():
                 authors.append(author["full_name"])
         work["authors"] = authors
+        work["provenance"] = "scienti"
+
         es_handler.insert_work(_id=str(response.inserted_id), work=work)
     else:
         if verbose > 4:
