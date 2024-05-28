f74cc29605375d2caac38cf2cd47c021d3b50241
Omar Zapata <andresete.chaos@gmail.com>
2024-05-28 17:13:48 -0500

    [kahi_wikidata_affiliations] fixed url

--- Kahi_wikidata_affiliations/kahi_wikidata_affiliations/Kahi_wikidata_affiliations.py
+++ Kahi_wikidata_affiliations/kahi_wikidata_affiliations/Kahi_wikidata_affiliations.py
@@ -47,8 +47,7 @@ def process_one(kahi_col, wikid_col, inst, verbose):
                 {"provenance": "wikidata", "source": "logo", "url": url_img})
     elif "P18" in rec["claims"].keys() and len(rec["claims"]["P18"]) > 0:
         img = rec["claims"]["P18"][0]["mainsnak"]["datavalue"]["value"]
-        url_img = f"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/{
-            img}&width=300"
+        url_img = f"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/{img}&width=300"
         inst["external_urls"].append(
             {"provenance": "wikidata", "source": "logo", "url": url_img})
     inst["updated"].append({"source": "wikidata", "time": int(time())})
