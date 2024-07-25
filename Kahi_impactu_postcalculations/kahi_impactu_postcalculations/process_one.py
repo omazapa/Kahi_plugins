104696ab3c015079bab5476a77a0f2a153bc21d7
Omar Zapata <andresete.chaos@gmail.com>
2024-07-25 14:02:17 -0500

    [kahi_impactu_postcalculations] refactored the plugin and added support for multiprocessing paralelization

--- Kahi_impactu_postcalculations/kahi_impactu_postcalculations/process_one.py
+++ Kahi_impactu_postcalculations/kahi_impactu_postcalculations/process_one.py
@@ -1,4 +1,19 @@
 from math import log, exp
+from pymongo import MongoClient
+from spacy import load
+
+# for multiprocessing have to be loaded global
+en_model = None
+es_model = None
+
+
+def load_nlp_models():
+    global en_model
+    global es_model
+    if not en_model:
+        en_model = load('en_core_web_sm')
+    if not es_model:
+        es_model = load('es_core_news_sm')
 
 
 def count_works_one(db, author_id):
@@ -18,11 +33,68 @@ def count_works_one(db, author_id):
         return author_id
 
 
-def network_creation_affiliations(colombia, impactu, idx, author_count):
-    already = impactu["affiliations"].find_one({"_id": idx})
+def network_creation_process_one(config, client, impactu_client, idx, author_count, net, backend):
+    """
+    Function to create the network of coauthorships for an affiliation or author.
+
+    Parameters:
+    ----------
+    config : dict
+        The configuration dictionary.
+    client : pymongo.database.Database
+        The database where the information is stored.
+    impactu_client : pymongo.database.Database
+        The database where the information will be stored.
+    idx : str
+        The affiliation or author identifier.
+    author_count : int
+        The maximum number of authors in a work to consider.
+    net : str
+        The network type, either affiliations or authors.
+    backend : str
+        The backend to use for the parallel processing. "mutiprocessing" or "threading".
+    """
+    if backend != "threading":
+        client = MongoClient(config["database_url"])
+        db_in = client[config["database_name"]]
+        impactu_client = MongoClient(
+            config["impactu_postcalculations"]["database_url"])
+        db_out = impactu_client[config["impactu_postcalculations"]
+                                ["database_name"]]
+    if net not in ["affiliations", "person"]:
+        print("ERROR: Invalid network type options are affiliations or authors")
+        return
+
+    if net == "affiliations":
+        network_creation_affiliations(db_in, db_out, idx, author_count)
+    if net == "person":
+        network_creation_person(db_in, db_out, idx, author_count)
+
+    if backend != "threading":
+        client.close()
+        impactu_client.close()
+
+
+def network_creation_affiliations(db_in, db_out, idx, author_count):
+    """
+    Function to create the network of coauthorships for an affiliation.
+
+    Parameters:
+    ----------
+    db_in : pymongo.database.Database (kahi dabatabase)
+        The database where the information is stored.
+    db_out : pymongo.database.Database (calculation database)
+        The database where the information will be stored.
+    idx : str
+        The affiliation identifier.
+    author_count : int
+        The maximum number of authors in a work to consider.
+    """
+    already = db_out["affiliations"].find_one(
+        {"_id": idx, "coauthorship_network": {"$exists": True}})
     if already:
         return None
-    aff_info = colombia["affiliations"].find_one({"_id": idx})
+    aff_info = db_in["affiliations"].find_one({"_id": idx})
     name = aff_info["names"][0]["name"]
     for n in aff_info["names"]:
         if n["lang"] == "es":
@@ -35,7 +107,7 @@ def network_creation_affiliations(colombia, impactu, idx, author_count):
     edges = []
     edges_coauthorships = {}
     works_count = 0
-    for work in colombia["works"].find({"authors.affiliations.id": idx, "author_count": {"$lte": author_count}}):
+    for work in db_in["works"].find({"authors.affiliations.id": idx, "author_count": {"$lte": author_count}}):
         works_count += 1
         work_nodes = [idx]
         work_edges = []
@@ -82,7 +154,7 @@ def network_creation_affiliations(colombia, impactu, idx, author_count):
     for node in nodes:
         if node == idx:
             continue
-        for work in colombia["works"].find({"$and": [{"authors.affiliations.id": node}, {"authors.affiliations.id": {"$ne": idx}}], "author_count": {"$lte": author_count}}):
+        for work in db_in["works"].find({"$and": [{"authors.affiliations.id": node}, {"authors.affiliations.id": {"$ne": idx}}], "author_count": {"$lte": author_count}}):
             for author in work["authors"]:
                 for aff in author["affiliations"]:
                     if aff["id"] == idx:
@@ -104,7 +176,8 @@ def network_creation_affiliations(colombia, impactu, idx, author_count):
     nodes_db = []
     for i, node in enumerate(nodes):
         degree = len([1 for i, j in edges if i == node or j == node])
-        size = 50 * log(1 + degree / (num_nodes - 1), 2) if num_nodes > 1 else 1
+        size = 50 * log(1 + degree / (num_nodes - 1),
+                        2) if num_nodes > 1 else 1
         nodes_db.append(
             {
                 "id": str(node),
@@ -139,7 +212,6 @@ def network_creation_affiliations(colombia, impactu, idx, author_count):
             size = 10 / (1 + exp(6 - 10 * edge["coauthorships"] / top))
             edge["size"] = size if size >= 1 else 1
     record = {
-        "_id": idx,
         "coauthorship_network": {
             "nodes": nodes_db,
             "edges": edges_db
@@ -147,7 +219,6 @@ def network_creation_affiliations(colombia, impactu, idx, author_count):
     }
     try:
         record_edges = {
-            "_id": idx,
             "coauthorship_network": {
                 "edges": edges_db
             }
@@ -157,25 +228,40 @@ def network_creation_affiliations(colombia, impactu, idx, author_count):
         record["coauthorship_network"]["edges"] = record["coauthorship_network"]["edges"][0:nedges]
 
         record_edges["coauthorship_network"]["nodes"] = record["coauthorship_network"]["edges"][nedges:]
-        impactu["affiliations"].insert_one(record)
-        impactu["affiliations_edges"].insert_one(record_edges)
+        db_out["affiliations"].update_one(
+            {"_id": idx, }, {"$set": record}, upsert=True)
+        db_out["affiliations_edges"].update_one(
+            {"_id": idx, }, {"$set": record_edges}, upsert=True)
     except Exception as e:
         print(f"too big network for id {record['_id']}", e)
 
 
-def network_creation_person(colombia, impactu, idx):
-    already = impactu["person"].find_one(
+def network_creation_person(db_in, db_out, idx, author_count):
+    """
+    Function to create the network of coauthorships for an author.
+
+    Parameters:
+    ----------
+    db_in : pymongo.database.Database (kahi dabatabase)
+        The database where the information is stored.
+    db_out : pymongo.database.Database (calculation database)
+        The database where the information will be stored.
+    idx : str
+        The author identifier.
+    """
+
+    already = db_out["person"].find_one(
         {"_id": idx, "coauthorship_network": {"$exists": True}})
     if already:
         return None
-    aff_info = colombia["person"].find_one({"_id": idx})
+    aff_info = db_in["person"].find_one({"_id": idx})
     name = aff_info["full_name"]
     nodes = [idx]
     nodes_labels = [name]
     edges = []
     edges_coauthorships = {}
     works_count = 0
-    for work in colombia["works"].find({"authors.id": idx, "author_count": {"$lte": 10}}):
+    for work in db_in["works"].find({"authors.id": idx, "author_count": {"$lte": author_count}}):
         works_count += 1
         work_nodes = [idx]
         work_edges = []
@@ -220,7 +306,7 @@ def network_creation_person(colombia, impactu, idx):
     for node in nodes:
         if node == idx:
             continue
-        for work in colombia["works"].find({"$and": [{"authors.id": node}, {"authors.id": {"$ne": idx}}], "author_count": {"$lte": 10}}):
+        for work in db_in["works"].find({"$and": [{"authors.id": node}, {"authors.id": {"$ne": idx}}], "author_count": {"$lte": author_count}}):
             for author in work["authors"]:
                 if author["id"] == idx:
                     print("Problem found")
@@ -241,7 +327,8 @@ def network_creation_person(colombia, impactu, idx):
     nodes_db = []
     for i, node in enumerate(nodes):
         degree = len([1 for i, j in edges if i == node or j == node])
-        size = 50 * log(1 + degree / (num_nodes - 1), 2) if num_nodes > 1 else 1
+        size = 50 * log(1 + degree / (num_nodes - 1),
+                        2) if num_nodes > 1 else 1
         nodes_db.append(
             {
                 "id": str(node),
@@ -267,7 +354,6 @@ def network_creation_person(colombia, impactu, idx):
               for e in edges_db]) if len(edges_db) > 0 else 1
     bot = min([e["coauthorships"]
               for e in edges_db]) if len(edges_db) > 0 else 1
-    # avg=mean([e["coauthorships"] for e in edges])
     for edge in edges_db:
         if abs(top - edge["coauthorships"]) < 0.01:
             edge["size"] = 10
@@ -276,17 +362,85 @@ def network_creation_person(colombia, impactu, idx):
         else:
             size = 10 / (1 + exp(6 - 10 * edge["coauthorships"] / top))
             edge["size"] = size if size >= 1 else 1
-    impactu["person"].update_one({"_id": idx}, {"$set": {"coauthorship_network": {
-                                 "nodes": nodes_db, "edges": edges_db}}}, upsert=True)
+    db_out["person"].update_one({"_id": idx}, {"$set": {"coauthorship_network": {
+        "nodes": nodes_db, "edges": edges_db}}}, upsert=True)
 
 
-def top_words_affiliations(colombia, impactu, aff, es_model, en_model, stopwords):
-    aff_db = impactu["affiliations"].find_one(
+def top_words_process_one(config, client, impactu_client, aff, stopwords, top_words, backend):
+    """
+    Function to create the network of coauthorships for an affiliation or author.
+
+    Parameters:
+    ----------
+    config : dict
+        The configuration dictionary.
+    client : pymongo.database.Database
+        The database where the information is stored.
+    impactu_client : pymongo.database.Database
+        The database where the information will be stored.
+    idx : str
+        The affiliation or author identifier.
+    author_count : int
+        The maximum number of authors in a work to consider.
+    net : str
+        The network type, either affiliations or authors.
+    backend : str
+        The backend to use for the parallel processing. "mutiprocessing" or "threading".
+    """
+    global en_model
+    global es_model
+    if backend != "threading":
+        client = MongoClient(config["database_url"])
+        db_in = client[config["database_name"]]
+        impactu_client = MongoClient(
+            config["impactu_postcalculations"]["database_url"])
+        db_out = impactu_client[config["impactu_postcalculations"]
+                                ["database_name"]]
+    if top_words not in ["affiliations", "affiliations_others", "person"]:
+        print(
+            f"ERROR: Invalid network type options are {["affiliations", "affiliations_others", "person"]}")
+        return
+
+    if top_words == "affiliations":
+        top_words_affiliations(
+            db_in, db_out, aff, es_model, en_model, stopwords)
+    if top_words == "affiliations_others":
+        top_words_affiliations_others(
+            db_in, db_out, aff, es_model, en_model, stopwords)
+    if top_words == "person":
+        top_words_person(db_in, db_out, aff, es_model, en_model, stopwords)
+    if backend != "threading":
+        client.close()
+        impactu_client.close()
+
+
+def top_words_affiliations(db_in, db_out, aff, es_model, en_model, stopwords):
+    """
+    Function to get the top words for an affiliation.
+
+    This algorithm gets the top words for an affiliation based on the titles of the works of the authors of the affiliation.
+
+    Parameters:
+    ----------
+    db_in : pymongo.database.Database (kahi dabatabase)
+        The database where the information is stored.
+    db_out : pymongo.database.Database (calculation database)
+        The database where the information will be stored.
+    aff : dict
+        The affiliation information.
+    es_model : spacy.lang.es.Spanish
+        The Spanish model for the NLP.
+    en_model : spacy.lang.en.English
+        The English model for the NLP.
+    stopwords : list
+        The list of stopwords to ignore.
+    """
+    aff_db = db_out["affiliations"].find_one(
         {"_id": aff["_id"], "top_words": {"$exists": 1}})
     if aff_db:
         return
     results = {}
-    for work in colombia["works"].find({"authors.affiliations.id": aff["_id"], "titles.title": {"$exists": 1}}, {"titles": 1}):
+    for work in db_in["works"].find({"authors.affiliations.id": aff["_id"], "titles.title": {"$exists": 1}}, {"titles": 1}):
         title = work["titles"][0]["title"].lower()
         lang = work["titles"][0]["lang"]
         if lang == "es":
@@ -309,22 +463,42 @@ def top_words_affiliations(colombia, impactu, aff, es_model, en_model, stopwords
     results = []
     for top in topN:
         results.append({"name": top[0], "value": top[1]})
-    aff_db = impactu["affiliations"].find_one({"_id": aff["_id"]})
+    aff_db = db_out["affiliations"].find_one({"_id": aff["_id"]})
     if aff_db:
-        impactu["affiliations"].update_one(
+        db_out["affiliations"].update_one(
             {"_id": aff["_id"]}, {"$set": {"top_words": results}})
     else:
-        impactu["affiliations"].insert_one(
+        db_out["affiliations"].insert_one(
             {"_id": aff["_id"], "top_words": results})
 
 
-def top_words_affiliations_others(colombia, impactu, aff, es_model, en_model, stopwords):
-    aff_db = impactu["affiliations"].find_one(
+def top_words_affiliations_others(db_in, db_out, aff, es_model, en_model, stopwords):
+    """
+    Function to get the top words for an affiliation for other than institutions, such as group, department, faculty.
+
+    This algorithm gets the top words for an affiliation based on the titles of the works of the authors of the affiliation.
+
+    Parameters:
+    ----------
+    db_in : pymongo.database.Database (kahi dabatabase)
+        The database where the information is stored.
+    db_out : pymongo.database.Database (calculation database)
+        The database where the information will be stored.
+    aff : dict
+        The affiliation information.
+    es_model : spacy.lang.es.Spanish
+        The Spanish model for the NLP.
+    en_model : spacy.lang.en.English
+        The English model for the NLP.
+    stopwords : list
+        The list of stopwords to ignore.
+    """
+    aff_db = db_out["affiliations"].find_one(
         {"_id": aff["_id"], "top_words": {"$exists": 1}})
     if aff_db:
         results = {}
-        for author in colombia["person"].find({"affiliations.id": aff["_id"]}):
-            for work in colombia["works"].find({"authors.id": author["_id"], "titles.title": {"$exists": 1}}):
+        for author in db_in["person"].find({"affiliations.id": aff["_id"]}):
+            for work in db_in["works"].find({"authors.id": author["_id"], "titles.title": {"$exists": 1}}):
                 title = work["titles"][0]["title"].lower()
                 lang = work["titles"][0]["lang"]
                 if lang == "es":
@@ -347,17 +521,37 @@ def top_words_affiliations_others(colombia, impactu, aff, es_model, en_model, st
         results = []
         for top in topN:
             results.append({"name": top[0], "value": top[1]})
-        impactu["affiliations"].update_one(
+        db_out["affiliations"].update_one(
             {"_id": aff["_id"]}, {"$set": {"top_words": results}})
 
 
-def top_words_person(colombia, impactu, aff, es_model, en_model, stopwords):
-    aff_db = impactu["person"].find_one(
+def top_words_person(db_in, db_out, aff, es_model, en_model, stopwords):
+    """
+    Function to get the top words for an author.
+
+    This algorithm gets the top words for an author based on the titles of the works of the author.
+
+    Parameters:
+    ----------
+    db_in : pymongo.database.Database (kahi dabatabase)
+        The database where the information is stored.
+    db_out : pymongo.database.Database (calculation database)
+        The database where the information will be stored.
+    aff : dict
+        The author information.
+    es_model : spacy.lang.es.Spanish
+        The Spanish model for the NLP.
+    en_model : spacy.lang.en.English
+        The English model for the NLP.
+    stopwords : list
+        The list of stopwords to ignore.
+    """
+    aff_db = db_out["person"].find_one(
         {"_id": aff["_id"], "top_words": {"$exists": 1}})
     if aff_db:
         return
     results = {}
-    for work in colombia["works"].find({"authors.id": aff["_id"], "titles.title": {"$exists": 1}}, {"titles": 1}):
+    for work in db_in["works"].find({"authors.id": aff["_id"], "titles.title": {"$exists": 1}}, {"titles": 1}):
         title = work["titles"][0]["title"].lower()
         lang = work["titles"][0]["lang"]
         if lang == "es":
@@ -380,9 +574,9 @@ def top_words_person(colombia, impactu, aff, es_model, en_model, stopwords):
     results = []
     for top in topN:
         results.append({"name": top[0], "value": top[1]})
-    aff_db = impactu["person"].find_one({"_id": aff["_id"]})
+    aff_db = db_out["person"].find_one({"_id": aff["_id"]})
     if aff_db:
-        impactu["person"].update_one(
+        db_out["person"].update_one(
             {"_id": aff["_id"]}, {"$set": {"top_words": results}})
     else:
-        impactu["person"].insert_one({"_id": aff["_id"], "top_words": results})
+        db_out["person"].insert_one({"_id": aff["_id"], "top_words": results})
