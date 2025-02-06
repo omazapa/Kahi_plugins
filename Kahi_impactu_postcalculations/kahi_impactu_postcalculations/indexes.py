1aa2192144be328d03159809aece1f288e857caf
Omar Zapata <andresete.chaos@gmail.com>
2025-02-06 13:16:56 -0500

    [kahi_impactu_postcalculations] fixed calls to indexes

--- Kahi_impactu_postcalculations/kahi_impactu_postcalculations/indexes.py
+++ Kahi_impactu_postcalculations/kahi_impactu_postcalculations/indexes.py
@@ -9,11 +9,13 @@ def create_indexes(db):
     db : pymongo.database.Database
         Database object to create indexes on. (kahi)
     """
-    db.works.createIndex({ "groups.id": 1 });
-    db.works.createIndex({ "source.id": 1 });
-    db.works.createIndex({"citations_count.source": 1, "citations_count.count": 1});
-    db.works.createIndex({"titles.source": 1, "titles.title": 1});
-    db.afilliations.createIndex({"products_count": -1});
-    db.person.createIndex({"products_count": -1});
-    db.works.createIndex({"types.source": 1, "types.type": 1, "types.code": 1});
-    db.works.createIndex({"open_access.open_access_status": 1});
+    db["works"].create_index({"groups.id": 1})
+    db["works"].create_index({"source.id": 1})
+    db["works"].create_index(
+        {"citations_count.source": 1, "citations_count.count": 1})
+    db["works"].create_index({"titles.source": 1, "titles.title": 1})
+    db["afilliations"].create_index({"products_count": -1})
+    db["person"].create_index({"products_count": -1})
+    db["works"].create_index(
+        {"types.source": 1, "types.type": 1, "types.code": 1})
+    db["works"].create_index({"open_access.open_access_status": 1})
