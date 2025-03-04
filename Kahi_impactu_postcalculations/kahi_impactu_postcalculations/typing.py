1e9cedd2a91fbbfd23e1454b597285d4a493ddca
Omar Zapata <andresete.chaos@gmail.com>
2025-03-03 20:14:32 -0500

    [Kahi_impactu_postcalculations] fixed code quality

--- Kahi_impactu_postcalculations/kahi_impactu_postcalculations/typing.py
+++ Kahi_impactu_postcalculations/kahi_impactu_postcalculations/typing.py
@@ -14,12 +14,13 @@ def get_scienti_string(work):
     str
         The scienti string of the work types
     """
-    t=""
-    for i,_t in enumerate(work["types"]):
-        t+=_t["code"]+": "+_t["type"]+" "
+    t = ""
+    for i, _t in enumerate(work["types"]):
+        t += _t["code"] + ": " + _t["type"] + " "
     return t.strip()
 
-def process_scienti(work,types,verbose=False):
+
+def process_scienti(work, types, verbose=False):
     """
     Get the scienti type from the work and return the colav type
 
@@ -38,14 +39,15 @@ def process_scienti(work,types,verbose=False):
         The colav type or an empty dictionary if type is not found
     """
     t = get_scienti_string(work)
-    colav_type = types[types["Tipo"]==t]["Tipo Colav"].values
-    if len(colav_type)>1 and verbose:
+    colav_type = types[types["Tipo"] == t]["Tipo Colav"].values
+    if len(colav_type) > 1 and verbose:
         print(f"WARNING: more than one type found for {t} = {colav_type}")
-    if len(colav_type)==1:
-        return {"provenance":"scienti","source":"impactu","type":colav_type[0]}
+    if len(colav_type) == 1:
+        return {"provenance": "scienti", "source": "impactu", "type": colav_type[0]}
     return {}
 
-def process_minciencias(work,types):
+
+def process_minciencias(work, types):
     """
     Get the minciencias type from the work and return the colav type
 
@@ -61,13 +63,14 @@ def process_minciencias(work,types):
     dict
         The colav type or an empty dictionary if type is not found
     """
-    t = work["types"][0]["type"]+": "+work["types"][1]["type"]
-    colav_type = types[types["Tipo"]==t]["Tipo Colav"].values
-    if len(colav_type)>1:
+    t = work["types"][0]["type"] + ": " + work["types"][1]["type"]
+    colav_type = types[types["Tipo"] == t]["Tipo Colav"].values
+    if len(colav_type) > 1:
         print(f"WARNING: more than one type found for {t} = {colav_type}")
-    if len(colav_type)==1:
-        return {"provenance":"minciencias","source":"impactu","type":colav_type[0]}
+    if len(colav_type) == 1:
+        return {"provenance": "minciencias", "source": "impactu", "type": colav_type[0]}
     return {}
+
 
 def process_others(source, types):
     """
@@ -85,18 +88,18 @@ def process_others(source, types):
     function
         The function to process the type source
     """
-    def process_source(work,types):
+    def process_source(work, types):
         t = work["types"][0]["type"]
-        colav_type = types[types["Tipo"]==t]["Tipo Colav"].values
-        if len(colav_type)>1:
+        colav_type = types[types["Tipo"] == t]["Tipo Colav"].values
+        if len(colav_type) > 1:
             print(f"WARNING: more than one type found for {t} = {colav_type}")
-        if len(colav_type)==1:
-            return {"provenance":source,"source":"impactu","type":colav_type[0]}
+        if len(colav_type) == 1:
+            return {"provenance": source, "source": "impactu", "type": colav_type[0]}
         return {}
     return process_source
 
 
-functors={}
+functors = {}
 functors["minciencias"] = process_minciencias
 functors["scienti"] = process_scienti
 functors["ciarp"] = process_others("ciarp")
@@ -104,7 +107,7 @@ functors["openalex"] = process_others("openalex")
 functors["scholar"] = process_others("scholar")
 
 
-def process_one(db,work,source,types,verbose=True):
+def process_one(db, work, source, types, verbose=True):
     """
     Process one work to get the colav type
 
@@ -122,13 +125,14 @@ def process_one(db,work,source,types,verbose=True):
         If True, print warnings
 
     """
-    df_filtered = types[types["Fuente"].str.contains(source, case=False, na=False)]
-    if len(work["types"])>1 and (source != "minciencias" or source != "scienti") and verbose:
+    df_filtered = types[types["Fuente"].str.contains(
+        source, case=False, na=False)]
+    if len(work["types"]) > 1 and (source != "minciencias" or source != "scienti") and verbose:
         print(f"WARNING: more than one type found for {source} = {work}")
 
-    colav_type = functors[source] (work,df_filtered) 
+    colav_type = functors[source](work, df_filtered)
     if colav_type:
-       db["works"].update_one({ "_id": work["_id"] }, 
-                              { "$push": { "types": colav_type } })
+        db["works"].update_one({"_id": work["_id"]},
+                               {"$push": {"types": colav_type}})
     elif verbose:
         print(f"WARNING: colav type not found for {work}")
