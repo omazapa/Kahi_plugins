2fd2f1033789b0f3484fafb12bc96b5178c00beb
fedevergara <fedevego15@gmail.com>
2025-12-03 10:43:40 -0500

    [Kahi_scimago_sources] Added Open Access and Open Access Diamond values to the records

--- Kahi_scimago_sources/kahi_scimago_sources/Kahi_scimago_sources.py
+++ Kahi_scimago_sources/kahi_scimago_sources/Kahi_scimago_sources.py
@@ -207,6 +207,12 @@ class Kahi_scimago_sources(KahiBase):
                     "order": int(sjr["Rank"]) if sjr["Rank"] else None,
                     "source": "Scimago hindex"
                 })
+                if sjr.get("Open Access") and sjr.get("Open Access Diamond"):
+                    entry["open_access"].append({
+                        "provenance": "scimago",
+                        "is_open_access": True if sjr.get("Open Access") == "Yes" else False,
+                        "open_access_diamond": True if sjr.get("Open Access Diamond") == "Yes" else False
+                        })
                 if sjr["SJR"]:
                     rank = ""
                     if isinstance(sjr["SJR"], str):
